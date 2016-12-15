31ee90ffdd9b6c5491ef84b685a8fa7be6b4a3bf
ahmedhelmy204 <ahmedhelmy204@hotmail.com>
2016-12-15 10:01:20 +0300

    ChapterOne - Profesional TDD using C#

---
+++ TDD_StringUtilities/StringUtilities.Tests/StringUtilitiesTests.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using StringUtilities;
+
+namespace StringUtilities.Tests
+{
+    [TestClass]
+    public class StringUtilitiesTests
+    {
+        [TestMethod]
+        public void ShouldFindOneYInMysterious()
+        {
+            string stringToCheck = "mysterious";
+            string stringToFind = "y";
+            int expectedResult = 1;
+
+            StringUtilitiesClass stringUtilitiesClass = new StringUtilitiesClass();
+
+            int actualResult = stringUtilitiesClass.CountOccurences(stringToCheck, stringToFind);
+
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [TestMethod]
+        public void ShouldFindTwoSInMysterious()
+        {
+            string stringToCheck = "mysterious";
+            string stringToFind = "s";
+            int expectedResult = 2;
+
+            StringUtilitiesClass stringUtilitiesClass = new StringUtilitiesClass();
+
+            int actualResult = stringUtilitiesClass.CountOccurences(stringToCheck, stringToFind);
+
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [TestMethod]
+        public void SearchShouldBeCaseInSensetive()
+        {
+            string stringToCheck = "mySterious";
+            string stringToFind = "s";
+            int expectedResult = 2;
+
+            StringUtilitiesClass stringUtilitiesClass = new StringUtilitiesClass();
+
+            int actualResult = stringUtilitiesClass.CountOccurences(stringToCheck, stringToFind);
+
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [TestMethod]
+        public void ShouldBeAbleToHandleNulls()
+        {
+            string stringToCheck = null;
+            string stringToFind = "s";
+            int expectedResult = -1;
+
+            StringUtilitiesClass stringUtilitiesClass = new StringUtilitiesClass();
+
+            int actualResult = stringUtilitiesClass.CountOccurences(stringToCheck, stringToFind);
+
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+    }
+}
---
+++ TDD_StringUtilities/StringUtilities/StringUtilitiesClass.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringUtilities
+{
+    public class StringUtilitiesClass
+    {
+        public int CountOccurences(string stringToCheck,string stringToFind)
+        {
+            if(string.IsNullOrEmpty(stringToCheck))
+            {
+                return -1;
+            }
+
+            char[] stringAsCharArray = stringToCheck.ToUpper().ToCharArray();
+            char stringToCheckForAsChar = stringToFind.ToUpper().ToCharArray()[0];
+
+            int occurenceCount = 0;
+
+            for(int characterIndex = 0; characterIndex <= stringAsCharArray.GetUpperBound(0); characterIndex++)
+            {
+                if(stringAsCharArray[characterIndex]==stringToCheckForAsChar)
+                {
+                    occurenceCount++;
+                }
+            }
+
+            return occurenceCount;
+        }
+    }
+}
